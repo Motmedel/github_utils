345573ad393b495d7aacff2ff3d627e911e925aa
vph <vph.persson@gmail.com>
2025-02-24 21:30:45 +0100

    Add constants

--- pkg/webhook/types/types.go
+++ pkg/webhook/types/types.go
@@ -1,5 +1,15 @@
 package types
 
+const (
+	HeaderGitHubHookID                     = "X-GitHub-Hook-ID"
+	HeaderGitHubEvent                      = "X-GitHub-Event"
+	HeaderGitHubDelivery                   = "X-GitHub-Delivery"
+	HeaderHubSignature                     = "X-Hub-Signature"
+	HeaderHubSignature256                  = "X-Hub-Signature-256"
+	HeaderGitHubHookInstallationTargetType = "X-GitHub-Hook-Installation-Target-Type"
+	HeaderGitHubHookInstallationTargetID   = "X-GitHub-Hook-Installation-Target-ID"
+)
+
 type Delivery struct {
 	HookId                     string
 	Event                      string
